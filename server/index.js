42283d4841c344fbea5dad3a789637945d770db7
samsonkaleti <kaletishyam@gmail.com>
2025-01-09 16:49:38 +0530

    Update CORS configuration to use production frontend URL

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // Replace with your frontend URL in production
+    origin: "https://edviron-assignment-samsonkaletis-projects.vercel.app", // Replace with your frontend URL in production
     methods: ["GET", "POST","PUT","DELETE"],
     credentials: true,
   })
